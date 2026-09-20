f567d7b881619e645fc3c27a1f9d06d573972c58
zainsafadi6-zain <zainsafadi6@gmail.com>
2026-09-20 05:49:15 -0400

    fix: update database schema and constraints

--- element.sh
+++ element.sh
@@ -32,4 +32,4 @@ else
   do
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   done
-fi
+fiSun 20 Sep 2026 05:49:05 AM EDT
