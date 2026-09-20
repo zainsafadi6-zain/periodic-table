73a88c0e1c9e3471d6c043cb6396b9ca6a3daa9c
zainsafadi6-zain <zainsafadi6@gmail.com>
2026-09-20 05:49:34 -0400

    feat: add missing elements to database

--- element.sh
+++ element.sh
@@ -33,3 +33,4 @@ else
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   done
 fiSun 20 Sep 2026 05:49:05 AM EDT
+Sun 20 Sep 2026 05:49:22 AM EDT
